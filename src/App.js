89de9549fa24273c6b4930b3b8f5907d3a6ca4ef
Rahab Molepo <167099620+RahabMMM@users.noreply.github.com>
2024-10-27 16:50:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Benoni" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://aesthetic-cucurucho-6595ae.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rahab Molepo
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
